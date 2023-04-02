c5f1e16e3b1d771e136ca7b8c4d79717ef381f98
Dermot Boyle <dermotboyled@gmail.com>
2023-04-02 12:11:40 +0200

    Initial Commit

---
+++ Weaver_Widget_Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weaver_Widget_Demo
+//
+//  Created by Dermot Boyle on 2/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weaver_Widget_Demo/Weaver_Widget_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weaver_Widget_DemoApp.swift
+//  Weaver_Widget_Demo
+//
+//  Created by Dermot Boyle on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Weaver_Widget_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
